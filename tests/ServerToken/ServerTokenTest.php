ce32a2b6cabacf72840c291f8f032fdcd7dcd9f8
Patompong Savaengsuk <pond@netearth.net>
2019-02-04 13:43:32 +0700

    Add class to test ServerToken.

---
+++ tests/ServerToken/ServerTokenTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HMRC\Test\ServerToken;
+
+use HMRC\ServerToken\ServerToken;
+use PHPUnit\Framework\TestCase;
+
+class ServerTokenTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_set_server_token()
+    {
+        $serverToken = uniqid();
+
+        ServerToken::getInstance()->set($serverToken);
+
+        $this->assertEquals($serverToken, ServerToken::getInstance()->get());
+    }
+}
